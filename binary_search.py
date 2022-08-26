9911357c7a9475e8f76b66ef9c76219b7798410b
Nikesh <nikeshdhanunda@gmail.com>
2022-08-26 11:30:12 +0530

    binary searching

--- binary_search.py
+++ binary_search.py
@@ -1,21 +1,27 @@
-from math import floor
-
-
+'''function for searching key in list by binary search'''
 def binary_search(list,key):
-    n=len(list)
-    mid=n//2
-    print(mid,list[mid])
-    if(list[mid]==key):
-        return mid
-    elif(list[mid]>key):
-        binary_search(list[0:mid-1],key)
+    n=len(list)            
+    mid=n//2        # mid is the mid index of list
+    # check list is empty or not
+    if(n==0):       
+        return -1
     else:
-        binary_search(list[mid+1:n],key)
-pos=-1
+        
+        if(list[mid]==key):
+            return mid
+        elif(list[mid]>key):
+            return binary_search(list[0:mid],key)
+        else:
+            return binary_search(list[mid+1:n],key)
+
+
 list=[1,2,3,4,5,6]
 key=1
 pos=binary_search(list,key)
+
 if(pos==-1):
     print("key not found") 
 else:
-    print("key found at ",pos)     
+    print("key found at ",pos)  
+
+   
